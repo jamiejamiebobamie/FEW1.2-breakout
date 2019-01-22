735eb219ed337ee2af56eb47e3069cb1b034b9a8
jamiejamiebobamie <jmccrory@vt.edu>
2019-01-22 13:16:57 -0800

    completed the 5th of ten tutorial sections

--- index.js
+++ index.js
@@ -72,11 +72,19 @@ function draw() {
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+    if(y + dy < ballRadius) {
+    dy = -dy;
+} else if(y + dy > canvas.height-ballRadius) {
+    if(x > paddleX && x < paddleX + paddleWidth) {
         dy = -dy;
     }
-
+    else {
+        alert("GAME OVER");
+        document.location.reload();
+    }
+}
     x += dx;
     y += dy;
 }
-setInterval(draw, 10);
+
+var interval = setInterval(draw, 10);
